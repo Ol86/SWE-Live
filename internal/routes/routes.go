a208030cf6fcd62a6977a520c1d67735dab25ec9
Paul Bräuninger <brpa1033@h-ka.de>
2026-06-19 16:28:56 +0200

    feat: Add routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,13 +1,17 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"SWE-Live/internal/handler"
 
-func SetupRoutes(router *gin.Engine) {
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(router *gin.Engine, memberReadHandler *handler.MemberReadHandler) {
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
 	rest := router.Group("/rest")
 	{
-
+		memberReadHandler.RegisterRoutes(rest)
 	}
 }
